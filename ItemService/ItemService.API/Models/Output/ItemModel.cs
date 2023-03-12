600b88f4ad9580c199052682fc38eeaf5e3ff14f
Alon Mordechai <58881462+alon-m@users.noreply.github.com>
2023-03-12 22:19:07 +0200

    Support Extra info

--- ItemService/ItemService.API/Models/Output/ItemModel.cs
+++ ItemService/ItemService.API/Models/Output/ItemModel.cs
@@ -7,4 +7,6 @@ public class ItemModel
     public string? Description { get; set; }
     public List<ItemImage> Images { get; set; } = Enumerable.Empty<ItemImage>().ToList();
     public string OwnerId { get; set; } = string.Empty;
+    public Dictionary<string, bool>? IncludedExtras { get; set; }
+
 }
