01c70f3e3959f1fc073f114c5decff89b54e47ba
Alon Mordechai <58881462+alon-m@users.noreply.github.com>
2023-03-20 20:20:34 +0200

    update nugets

---
+++ Boro.EntityFramework/DbContexts/BoroMainDb/Tables/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boro.EntityFramework.DbContexts.BoroMainDb.Tables
+{
+    public class Users
+    {
+        [Key]
+        public string FacebookId { get; set; } = string.Empty;
+        public string FacebookAccessToken { get; set; } = string.Empty;
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
